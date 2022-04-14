0fc415be8fef3f7adfb3fbeb42336a8efc9fd3c5
319207 <319207@SERVER3>
2022-04-14 11:07:13 +0300

    Добавили конструкторы

--- Pr1/Pr1/Obyv.cs
+++ Pr1/Pr1/Obyv.cs
@@ -6,8 +6,20 @@ namespace Pr1
 {
     class Obyv
     {
-        private string Proizv;
-        private float Razmer;
-        private double Cena;
+        private string Proizv { get; set; }
+        private float Razmer { get; set; }
+        private double Cena { get; set; }
+        public Obyv()
+        {
+            Proizv = "";
+            Razmer = 0;
+            Cena = 0;
+        }
+        public Obyv(string proizv, float razmer, double cena)
+        {
+            this.Cena = cena;
+            this.Proizv = proizv;
+            this.Razmer = razmer;
+        }
     }
 }
